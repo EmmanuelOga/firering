3fd2d03ed226da6271f68ffbd4f88a60c3e123b4
Emmanuel Oga <EmmanuelOga@gmail.com>
2010-05-20 01:11:04 -0300

    updated spec

--- spec/data_spec.rb
+++ spec/data_spec.rb
@@ -6,18 +6,14 @@ end
 
 describe Firering::Data do
 
-  it "should infer the basekey from the class name" do
-    TestData.base_key.should == "testdata"
-  end
-
   it "can be initialized from a hash" do
-    object = TestData.new({"testdata" => { "a" => 1, "b" => 2}})
+    object = TestData.new({:testdata => { :a => 1, :b => 2}}, :testdata)
     object.a.should == 1
     object.b.should == 2
   end
 
   it "can be initialized from a json string" do
-    object = TestData.new('{"testdata" : { "a" : 1, "b" : 2}}')
+    object = TestData.new('{"testdata" : { "a" : 1, "b" : 2}}', :testdata)
     object.a.should == 1
     object.b.should == 2
   end
